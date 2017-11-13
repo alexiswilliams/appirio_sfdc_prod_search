7c53a38daf6f9a670d4cdf14d969bd5c2a070052
Alexis Williams <lex@appirio.com>
2017-11-13 10:48:19 -0500

    Update to include CMC Search
    search against issues, tasks, and user stories not OOTB with the URL from the AppirioDX team. If there's not a match, you will get an error message. Just use the global search instead.

--- background.js
+++ background.js
@@ -3,11 +3,13 @@
 // found in the LICENSE file.
 
 'use strict';
+// Add a listener to create the initial context menu items,
+// context menu items only need to be created at runtime.onInstalled
 chrome.runtime.onInstalled.addListener(function() {
-  for (let key of Object.keys(kSearchEngineIDValues)) {
+  for (let key of Object.keys(kURLSearchValues)) {
     chrome.contextMenus.create({
       id: key,
-      title: kSearchEngineIDValues[key] + " for %s'",
+      title: kSearchValues[key] + " for '%s'",
       type: 'normal',
       contexts: ['selection'],
     });
@@ -15,6 +17,6 @@ chrome.runtime.onInstalled.addListener(function() {
 });
 
 chrome.contextMenus.onClicked.addListener(function(item, tab) {
-  let url = kSearchEngineURLValues[item.menuItemId] + item.selectionText;
+  let url = kURLSearchValues[item.menuItemId] + item.selectionText;
   chrome.tabs.create({url: url, index: tab.index + 1});
 });
--- searches.js
+++ searches.js
@@ -1,9 +1,9 @@
-const kSearchEngineIDValues = {
+const kURLSearchValues = {
   'CMCsearchStoriesTasksandIssues' : 'https://appirio--c.na3.visual.force.com/apex/CMC_Redirect?cmc_number=',
   'GlobalSFDCSearch' : 'https://appirio.my.salesforce.com/_ui/search/ui/UnifiedSearchResults?searchType=2&str='
 };
 
-const kSearchEngineURLValues = {
+const kSearchValues = {
   'CMCsearchStoriesTasksandIssues' : 'CMC search Stories, Tasks, and Issues',
-  'GlobalSFDCSearch' : 'Global SFDC Search'
+  'GlobalSFDCSearch' : 'Global Appirio SFDC search'
 };
